c29c8ba1290c7c572a2cd2300e5eadee9c98b6a4
Mikita Isakau <harley26.gold@gmail.com>
2017-03-20 16:04:53 +0300

    -Created index.html in calculator-task -Deleted extra lines -Working integerDivide() function

--- calculator-task/calc.js
+++ calculator-task/calc.js
@@ -11,44 +11,16 @@ function divide(a,b) {
     return a / b
 }
 function integerDivide(a,b) {
-    return Math.floor(a / b);
+    return a / b >> 0;
 }
-
-console.log("Without parameters:");
 console.log(addition() + '\n');
-
-console.log("With one parameter:");
 console.log(addition(1) + '\n');
-
-console.log("Additions:");
 console.log(addition(10, 10));
-console.log(addition(20, "ten"));
 console.log(addition("twenty", true));
-console.log(addition(false, true));
-console.log(addition(true, 10));
-
-console.log("Subtractions:");
 console.log(subtraction(10, 10));
-console.log(subtraction("twenty", true));
 console.log(subtraction(true, 10));
-
-console.log("Multiplications:");
 console.log(multiple(10, 10));
 console.log(multiple("twenty", true));
-console.log(multiple(false, true));
-console.log(multiple(true, 10));
-
-console.log("Divisions:");
 console.log(divide(100, 10));
 console.log(divide(20, "ten"));
-console.log(divide(false, true));
-
-console.log("Integer divisions:");
 console.log(integerDivide(104, 10));
-console.log(integerDivide(20, "ten"));
-
-
-
-
-
-
