be45562a344ef535a150d2934f74a5264cb7909b
Mikita Isakau <harley26.gold@gmail.com>
2017-03-27 18:50:42 +0400

    Some edits have been made

--- array-task/arrays_operations.js
+++ array-task/arrays_operations.js
@@ -7,45 +7,26 @@ function isArrayTwo(arr) {
   return arr instanceof Array;
 }
 
-function isArrayThree(arr) {
-  return Object.prototype.toString.call(arr) === '[object Array]'
-}
-
-function range() {
+function range(x, y, z) {
   var arr = [];
-  switch (arguments.length){
-    case 1 : {
-      for (var i = 0; i < arguments[0]; i++) {
-        arr.push(i);
-      }
-    } break;
-    case 2 : {
-      for (var i = arguments[0]; i < arguments[1]; i++) {
-        arr.push(i);
-      }
-    } break;
-    case 3 : {
-      for (var i = arguments[0]; i < arguments[1]; i++) {
-        if (i % (arguments[2]) !== 0) {
-          arr.push(i)
-        }
-      }
-    } break;
+  if (y === undefined && z === undefined){ y = x; x = 0;  }
+  for (var i = x; i < y; i++) {
+    if (i % (z) !== 0) { arr.push(i) }
   }
   return arr;
 }
 
 function compact(x) {
   return x.filter(function (x) {
-    return Boolean(x).valueOf() === true;
+    return x;
   });
 }
 
 function sumWithCycle(x) {
   var sum = 0;
-  x.forEach(function (item) {
-    sum += item;
-  });
+  for (var i =0; i < x.length; i++) {
+    sum += x[i];
+  }
   return sum;
 }
 
@@ -57,11 +38,11 @@ function sumWithoutCycle(x) {
 
 function uniqueWithCycle(x){
   var arr = [];
-  x.forEach(function (item) {
-    if (arr.indexOf(item) === -1){
-      arr.push(item)
+  for (var i =0; i < x.length; i++) {
+    if (arr.indexOf(x[i]) === -1){
+      arr.push(x[i])
     }
-  });
+  }
   return arr;
 }
 
@@ -72,26 +53,19 @@ function uniqueWithoutCycle(x) {
 }
 
 function last(x) {
-  return x[(x.length - 1)];
+  return x[x.length - 1];
 }
 
-function excludeLastWithCycle(arr, x) {
-  if (x === undefined) {
-    arr.pop();
-  }
-  for (var i = 0; i < x; i++) {
-    arr.pop()
-  }
-  return arr;
+function lastSecond(x) {
+  return x.reverse()[0]
 }
 
-function excludeLastWithoutCycle(arr, x) {
+function excludeLast(arr, x) {
   if (x !== undefined) {
-    arr.length -= x;
+    return arr.slice(0, arr.length - x)
   } else {
-    arr.length -= 1;
+    return arr.slice(0, arr.length - 1)
   }
-  return arr;
 }
 
 console.log("isArray():");
@@ -99,31 +73,21 @@ console.log(isArrayOne([]));
 console.log(isArrayOne({}));
 console.log(isArrayTwo([]));
 console.log(isArrayTwo({}));
-console.log(isArrayThree([]));
-console.log(isArrayThree({}));
-
 console.log("\nrange():");
-console.log(range(10));
-console.log(range(5,10));
+console.log(range(5));
+console.log(range(1,5));
 console.log(range(1,10,2));
-
 console.log("\ncompact()");
 console.log(compact([{}, false, 1, 0]));
-
 console.log("\nSum:");
 console.log(sumWithCycle([1,2,3,4,5,6,7,8,9,10]));
 console.log(sumWithoutCycle([1,2,3,4,5,6,7,8,9,10]));
-
 console.log("\nUnique:");
 console.log(uniqueWithCycle([1,1,1,1,2,1,1,2,3,6,5,4,4,4]));
 console.log(uniqueWithoutCycle([1,1,1,1,2,1,1,2,3,6,5,4,4,4]));
-
 console.log("\nLast:");
 console.log(last([1,2,3,4,5,6]));
-
+console.log(lastSecond([1,2,3,4,5,6]));
 console.log('\nexcludeLast with array [1,1,1,1,2,1,1,2,3,6,5,4,4,4] and count 5');
-console.log(excludeLastWithCycle([1,1,1,1,2,1,1,2,3,6,5,4,4,4], 5));
-console.log(excludeLastWithCycle([1,1,1,1,2,1,1,2,3,6,5,4,4,4]));
-
-console.log(excludeLastWithoutCycle([1,1,1,1,2,1,1,2,3,6,5,4,4,4], 5));
-console.log(excludeLastWithoutCycle([1,1,1,1,2,1,1,2,3,6,5,4,4,4]));
+console.log(excludeLast([1,1,1,1,2,1,1,2,3,6,5,4,4,4], 5));
+console.log(excludeLast([1,1,1,1,2,1,1,2,3,6,5,4,4,4]));
